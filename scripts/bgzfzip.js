6ca9f15d3f2ada481df8555687883507faec4c8f
Jason Weirather <jason.weirather@gmail.com>
2016-12-03 18:32:00 -0600

    add bgzf

--- scripts/bgzfzip.js
+++ scripts/bgzfzip.js
@@ -8,26 +8,28 @@ var main = function (args) {
   var inf = process.stdin;
   if (args.input !== '-') {
     inf = fs.createReadStream(args.input);
-  } 
+  }
   // Setup Outputs
   var of = process.stdout;
-  console.log(args);
-  //var zip = bgzf.createBGZFCompress({level:args.level});
-  //inf.pipe(zip).pipe(of);
-
+  if (args.output) {
+    of = fs.createWriteStream(args.output);
+  }
+  var zip = bgzf.createBGZFCompress({level:args.level});
+  inf.pipe(zip).pipe(of);
 }
 
 var do_inputs = function () {
   var parser = new ArgumentParser({
     version:'0.0.1',
     addHelp:true,
-    description:'BGZF compress a file.'
+    description:'BGZF compress a file.',
+    formatterClass:ArgumentParser.ArgumentsDefaultsHelpFormatter
   });
 
   //Add arguments
   parser.addArgument(['input'],{help:"File to compress or - for STDIN"});
   parser.addArgument(['-o','--output'],{help:"Specify a file, otherwise output to STDOUT"});
-  parser.addArgument(['-l','--level'],{help:"Set compression level",type:'int',default:9});
+  parser.addArgument(['-l','--level'],{help:"Set compression level",type:'int',defaultValue:9});
   var args =  parser.parseArgs();
   return args;
 }
