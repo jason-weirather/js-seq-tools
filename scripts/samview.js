133416ed848da4e8b902a2b1a51e2247e56cfeb7
Jason Weirather <jason.weirather@gmail.com>
2016-12-10 15:31:16 -0600

    now we have working samtools view like utility

--- scripts/samview.js
+++ scripts/samview.js
@@ -3,6 +3,7 @@
 const ArgumentParser = require('argparse').ArgumentParser;
 const bgzf = require('../index.js').formats.compression.bgzf;
 const bam = require('../index.js').formats.alignment.bam;
+const sam = require('../index.js').formats.alignment.sam;
 const fs = require('fs');
 
 // SIMILAR TO SAMTOOLS VIEW IN FUNCTION
@@ -17,34 +18,59 @@ var main = function (args) {
     inf = fs.createReadStream(args.input);
   }
   // Setup Outputs
-  var of = process.stdout;
-  if (args.output) {
-    of = fs.createWriteStream(args.output);
+  var of;
+  var outfile = process.stdout;
+  if (args.output) outfile = fs.createWriteStream(args.output);
+  let bgzfz;
+  if (args.bam_output) {
+    // bamoutput
+    bgzfz = new bgzf.BGZFCompress();
+    of = bgzfz.pipe(outfile);
+  } else {
+    // basic non-bam output
+    of = outfile;
   }
+
   //var bamconv = new bam.BAMInputStream();
-  var bgzfun = new bgzf.BGZFDecompress();
-  var bamconv = new bam.DecompressedToBAMObj();
-  inf.pipe(bgzfun).pipe(bamconv).on('data',function (indata) {
-    if (indata.header && (args.header || args.header_only)) { 
+  let p;
+  var samconv;
+  var bgzfun;
+  var bamconv;
+  if (args.sam_input) { 
+    samconv = new sam.DataToSAMObj();
+    p = inf.pipe(samconv);
+  } else { 
+    bgzfun = new bgzf.BGZFDecompress();
+    bamconv = new bam.DecompressedToBAMObj(); 
+    p = inf.pipe(bgzfun).pipe(bamconv);
+  }
+  p.on('data',function (indata) {
+    // do our header if we are on our header
+    if (args.bam_output && indata.header) {  
+      // if its a bam output we always need header
+      bgzfz.write(indata.header.bam_data);
+    } else if (indata.header && (args.header || args.header_only)) { 
       of.write(''+indata.header+"\n"); 
       if (args.header_only) {
         this.push(null);
         return;
       }
     }
-    if (indata.bam) { 
+    // Do our data for sam or bam if we have it
+    if (indata.bam || indata.sam) { 
+      let data = indata.bam || indata.sam;
+
+      // see if we need to filter it based on flags
       let use_this = true;
       if (args.flag_remove || args.flag_retain) {
         let check;
         if (args.flag_retain) { 
-          check = indata.bam.flag & args.flag_retain;
+          check = data.flag & args.flag_retain;
           use_this = false; 
         } else if (args.flag_remove) {
-          check = indata.bam.flag & args.flag_remove;
+          check = data.flag & args.flag_remove;
           use_this = true;
         }
-        //console.log(indata.bam.flag);
-        //console.log(args.flag_remove);
         if (check !== 0 && args.flag_remove) {
           use_this = false;
         }
@@ -52,9 +78,22 @@ var main = function (args) {
           use_this = true;
         }
       }
-      if (use_this) of.write(''+indata.bam+"\n"); 
+
+      // check for filter condition and output
+      if (use_this) {
+        if (! args.bam_output) of.write(''+data+"\n"); 
+        else bgzfz.write(data.bam_data);
+      } 
     }
   });
+  // in the a case of bam listen for when to close out or writing pipes.
+  if (args.bam_output) {
+    let conv = samconv || bamconv;
+    conv.on('end',function () {
+      bgzfz.end();
+      if (args.output) { of.end(); }
+    });
+  }
 }
 
 var do_inputs = function () {
@@ -68,12 +107,12 @@ var do_inputs = function () {
   //Add arguments
   parser.addArgument(['input'],{help:"BAM to extract or - for STDIN"});
   parser.addArgument(['-o','--output'],{help:"Specify a file, otherwise output to STDOUT"});
-  parser.addArgument(['-b','--bam_output'],{help:"Output in bam format"});
+  parser.addArgument(['-b','--bam_output'],{help:"Output in bam format",action:'storeTrue'});
   parser.addArgument(['-t','--header'],{help:"Include the header",action:'storeTrue'});
   parser.addArgument(['-T','--header_only'],{help:"Only output the header",action:'storeTrue'});
   parser.addArgument(['-f','--flag_retain'],{help:"remove entries with this flag set",type:'int'});
   parser.addArgument(['-F','--flag_remove'],{help:"retain entries with this flag set",type:'int'});
-  parser.addArgument(['-S','--sam_input'],{help:"input is a SAM format"});
+  parser.addArgument(['-S','--sam_input'],{help:"input is a SAM format",action:'storeTrue'});
   var args =  parser.parseArgs();
   return args;
 }
